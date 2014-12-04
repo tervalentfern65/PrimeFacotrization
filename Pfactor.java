5367d5e450eb91e3a0cfdfb5853ad220233eb039
tervalentfern65 <joey1999tom@gmail.com>
2014-12-04 10:12:16 -0500

    pfactor
    what

---
+++ Pfactor.java
@@ -0,0 +1,18 @@
+public class PrimeFactor
+{
+
+    public static void main(String[] args)
+    {
+        int num = 20;
+        primeFactorization(num);
+    }
+
+    public static boolean isPrime(int num)
+    {
+
+    }
+
+    Public static String primeFactorization(int num)
+{}
+
+}
